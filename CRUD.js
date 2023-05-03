858e5d3f30858801134858699df82eb3f1605f9c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-02 22:42:22 -0300

    JS

---
+++ CRUD.js
@@ -0,0 +1,42 @@
+document.querySelector("#salvar").addEventListener("click", cadastrar)
+
+function cadastrar(){
+    let titulo = document.querySelector("#titulo").value
+    let descricao = document.querySelector("#descricao").value
+    let pontos = document.querySelector("#pontos").value
+    let categoria = document.querySelector("#categoria").value
+
+    const tarefa = {
+        titulo,
+        descricao,
+        pontos,
+        categoria,
+    }
+
+    document.querySelector("#tarefas").innerHTML += gerarCard(titulo)
+}
+
+
+
+function gerarCard(tarefa){
+    return `<div class="col-12 col-md-6 col-lg-4">
+    <div class="card">
+        <div class="card-header">
+            ${tarefa.titulo}
+        </div>
+        <div class="card-body">
+            <p class="card-text">${tarefa.descricao}</p>
+            <p>
+                <span class="badge text-bg-warning">${tarefa.categoria}</span>
+            </p>
+            <p>${tarefa.pontos}pts</p>
+            <a href="#" class="btn btn-success">
+                <i class="bi bi-check-lg"></i>
+            </a>
+            <a href="#" class="btn btn-danger">
+                <i class="bi bi-trash"></i>
+            </a>
+        </div>
+    </div> <!-- card -->
+</div> <!-- col -->`
+}
